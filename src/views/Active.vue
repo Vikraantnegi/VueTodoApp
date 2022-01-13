efebfd075fea5760bc9cb741084d304e091e4eee
Vikrant Negi <43957569+Vikraantnegi@users.noreply.github.com>
2022-01-13 19:35:16 +0530

    Fix Active route bugs

--- src/views/Active.vue
+++ src/views/Active.vue
@@ -8,13 +8,10 @@
       <div class="homeContent__head">My Tasks</div>
     </div>
     <div class="homeContent__tasks">
-        <div class="todo" v-for="(task, index) in current" :key="task.id">
+        <div class="todo" v-for="(task, index) in todosFiltered" :key="task.id">
             <div class="todo__head">
-                <div @click="changeStatus(index)" v-show="task.done" class="doneIcon grow pointer">
-                    <i class="fas fa-check-circle"></i>
-                </div>
                 <div class="todo__actions">
-                    <i @click="editTodo(index)" class="fas fa-pen-square editLogo grow pointer"></i>
+                    <i @click="editTodo(task)" class="fas fa-pen-square editLogo grow pointer"></i>
                     <i @click="deleteTodo(index)" class="fas fa-trash deleteLogo grow pointer"></i>
                 </div>
             </div>
@@ -40,7 +37,7 @@ export default {
   },
   data: () => ({
     date: 'January 13, 2022',
-    incomplete: 3,
+    incomplete: 0,
     current: [],
     list: [{
           "id": 1,
@@ -90,23 +87,25 @@ export default {
       },
     ],
   }),
+  computed: {
+    todosFiltered(){
+      return this.current.filter(task => !task.done)
+    }
+  },
   methods: {
-    changeStatus: function(index){
-      this.current[index].done = !this.current[index].done
-    },
     deleteTodo: function(index){
-      this.current.splice(index, 1);
+      this.current.splice(index, 1)
     },
-    editTodo: function(){
-
+    editTodo: function(task){
+      this.$router.push({name: 'Add', params: task})
     }
   },
   created(){
     this.current = this.list.filter(task => !task.done)
-    this.incomplete = this.current.length
+    this.incomplete = this.list.filter(task => !task.done).length
   },
   updated(){
-    this.incomplete = this.current.length
+    this.incomplete = this.list.filter(task => !task.done).length
   }
 }
 </script>
