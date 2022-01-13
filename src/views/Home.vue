d9ed637be78edf031704e2e6a0bdea945f653b32
Vikrant Negi <43957569+Vikraantnegi@users.noreply.github.com>
2022-01-13 14:14:51 +0530

    Home Content and Filters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,23 @@
       <div class="homeDate">{{ date }}</div>
       <div class="tasksStatus">{{ incomplete }} Unfinished, {{ completed }} Finished </div>
     </div>
+    <div class="homeContent">
+      <div class="homeContent__head">My Tasks</div>
+      <div class="homeContent__buttons">
+        <div @click="filter('all')" class="filterButton grow">
+          All Tasks
+        </div>
+        <div @click="filter('incomplete')" class="filterButton grow">
+          Incomplete Tasks
+        </div>
+        <div @click="filter('completed')" class="filterButton grow">
+          Completed Tasks
+        </div>
+      </div>
+      <div class="homeContent__tasks">
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +34,11 @@ export default {
     incomplete: 3,
     completed: 3,
   }),
+  methods: {
+    filter: function(val){
+      console.log(val)
+    }
+  },
 }
 </script>
 
@@ -37,5 +59,27 @@ export default {
   font-weight: 600;
   font-size: 18px;
   color: #575767
+}
+.homeContent{
+  padding: 20px 0;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.homeContent__head{
+  font-weight: bold;
+  font-size: 28px;
+}
+.homeContent__buttons{
+  display: flex;
+  flex-direction: row;
+  padding: 0 30px;
+  gap: 20px;
+}
+.filterButton{
+  padding: 12px 16px;
+  border: 1px solid #FFFFFF;
+  border-radius: 8px;
 }
 </style>
