e31da7552ee018859b3406f512fa150fcc2172e9
Vikrant Negi <43957569+Vikraantnegi@users.noreply.github.com>
2022-01-13 12:09:34 +0530

    Remove unused files

--- todo.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {"title":"HTML I","done":true, "description": "lorem ipsum"},
-    {"title":"CSS","done":true, "description": "lorem ipsum"},
-    {"title":"Responsive design","done":false, "description": "lorem ipsum"},
-    {"title":"Git","done":false, "description": "lorem ipsum"},
-    {"title":"JavaScript I","done":true, "description": "lorem ipsum"},
-    {"title":"JavaScript II","done":false, "description": "lorem ipsum"}
-]
